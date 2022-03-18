51776269dc02a0e1664017bced7c2df43c2dcf28
appliedrd <ted@appliedrd.com>
2022-03-17 21:23:15 -0400

    start of read csv file

---
+++ inc/FileProcess.h
@@ -0,0 +1,9 @@
+//
+// Created by ted on 2022-03-17.
+//
+
+#ifndef WW_C_STEPANALYZER_FILEPROCESS_H
+#define WW_C_STEPANALYZER_FILEPROCESS_H
+
+#endif //WW_C_STEPANALYZER_FILEPROCESS_H
+void readCSV(float *z , char *filename);
---
+++ src/FileProcess.c
@@ -0,0 +1,34 @@
+//
+// Created by ted on 2022-03-17.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+const char* getfield(char* line, int num)
+{
+    const char* tok;
+    for (tok = strtok(line, ";");
+         tok && *tok;
+         tok = strtok(NULL, ";\n"))
+    {
+        if (!--num)
+            return tok;
+    }
+    return NULL;
+}
+
+void readCSV(float *z , char *filename)
+{
+    FILE* stream = fopen(filename, "r");
+
+    char line[1024];
+    while (fgets(line, 1024, stream))
+    {
+        char* tmp = strdup(line);
+        printf("Field 3 would be %s\n", getfield(tmp, 3));
+        // NOTE strtok clobbers tmp
+        free(tmp);
+    }
+}
+
